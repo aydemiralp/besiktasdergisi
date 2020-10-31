17f65f31416f428f17c843411bbd7aee7c023f00
Alp Aydemir <68555108+aydemiralp@users.noreply.github.com>
2020-10-31 20:41:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cardviewexample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cardviewexample
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+    }
+}
